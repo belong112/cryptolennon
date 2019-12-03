ca04b95bd310c034e734b04bf4e11a4c45bcd71b
b05901172 <b05901172>
2019-12-03 22:42:02 +0800

    fixed

--- contracts/Lennon.sol
+++ contracts/Lennon.sol
@@ -33,7 +33,7 @@ contract Lennon {
 
     function createAccount(string memory _name, uint8 _d, uint8 _m, uint16 _y) public {
         require(keccak256(abi.encodePacked(_name)) != keccak256(abi.encodePacked("")), "Name cannot be empty");
-        require(owner_to_id[msg.sender] != 0, "Cannot create more than one account");
+        require(owner_to_id[msg.sender] == 0, "Cannot create more than one account");
         uint id = Accounts.push(Account(_name, _d, _m, _y)) - 1;
         owner_to_id[msg.sender] = id;
         emit newAccount(id, _name, _d, _m, _y);
