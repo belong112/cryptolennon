2e002a361cbb77fed1867412e87202c8e604134f
belong112 <belong@DESKTOP-RHF6GUP.localdomain>
2019-12-29 10:11:05 +0800

    test

--- contracts/Lennon.sol
+++ contracts/Lennon.sol
@@ -62,8 +62,8 @@ contract Lennon is Ownable {
         petition_threshold = 3;
 
         Questions.push(Question("你支持反送中嗎?", "暴政，暴民?", 0, 0, new uint[](0)));
-        Questions.push(Question("你喜歡吃香菜嗎?", "真香", 0, 0, new uint[](0)));
-        Questions.push(Question("2020。歸。投。韓下去?", "1/11記得投票", 0, 0, new uint[](0)));
+        Questions.push(Question("你喜歡吃香菜嗎?", "真香", 1, 0, new uint[](0)));
+        Questions.push(Question("2020。歸。投。韓下去?", "1/11記得投票", 2, 0, new uint[](0)));
 
         /*
         Accounts.push(Account("鄭禕仁", 1, 2, 1987));
